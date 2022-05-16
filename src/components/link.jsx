b8c5e7f9691550e5e8963655070125ab525c52f9
aakarsh604 <aakarsh604@gmail.com>
2022-05-17 00:51:06 +0530

    navabr

---
+++ src/components/link.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Link = () => {
+
+    return (
+        <div className="links">
+        <div>Services</div>
+        <div>Projects</div>
+        <div>About</div>
+        </div>
+    )
+};
+
+export {Link};
